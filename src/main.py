5238783567aca46e0ba5e6923bbce5ba1972d03e
Zeke Barefoot <syncorex0@gmail.com>
2023-11-22 08:14:36 +0000

    Flask hello world page testing fix

--- src/main.py
+++ src/main.py
@@ -32,4 +32,6 @@ def index() -> str:
     return '<h1>Web App with Python Flask!</h1>'
 
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    # Start the Flask app
+    app.run(host='0.0.0.0', port=5000)
