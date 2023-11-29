2cb66ed0468b0690f13e4b022af5a1a870d135a1
Zeke Barefoot <syncorex0@gmail.com>
2023-11-29 00:11:52 +0000

    Use OpenTrivia API for questions, session score tracking

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
 from typing import Any
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from pymongo import MongoClient
 from pymongo.database import Database
 from pymongo.collection import Collection
 from pymongo.server_api import ServerApi
+import requests
+import random
+import html
 
 questions = [
     {
@@ -20,6 +23,7 @@ questions = [
 ]
 
 app = Flask(__name__)
+app.secret_key = 'BLABLABLA'
 
 cert_path = '/home/user/advpy-web-app/X509-cert-8255257794010601158.pem'
 
@@ -38,7 +42,7 @@ db = client['trivia']
 collection: Collection[Any]
 collection = db['questions']
 doc_count = collection.count_documents({})
-print(doc_count)
+# print(doc_count)
 
 
 @app.route('/')
@@ -48,31 +52,50 @@ def index() -> str:
 
 @app.route('/start')
 def start() -> Any:
+    session['score'] = 0
+    res = requests.get('https://opentdb.com/api.php?amount=10').json()
+    # print(res)
+    qs = list()
+    for q in res['results']:
+        q['options'] = q['incorrect_answers']
+        q['options'].insert(random.randint(0, len(q['incorrect_answers'])), q['correct_answer'])
+        q['question'] = html.unescape(q['question'])
+        qs.append(q)
+    session['questions'] = qs
+    # print(session['score'])
     return redirect(url_for('question', question_number=0))
 
+@app.route('/end')
+def end() -> Any:
+    return render_template('end.html', score=session['score'])
 
 @app.route('/question/<int:question_number>', methods=['GET', 'POST'])
 def question(question_number: int) -> Any:
+
+
     if request.method == 'POST':
         user_answer = request.form['answer']
-        correct_answer = questions[question_number]['correct_answer']
+        correct_answer = session['questions'][question_number]['correct_answer']
         if user_answer == correct_answer:
+            session['score'] += 1
             result_message = "Correct!"
         else:
             result_message = "Wrong!"
 
         return render_template('result.html',
+                               score=session['score'],
                                question_number=question_number,
                                correct_answer=correct_answer,
                                user_answer=user_answer,
                                result_message=result_message,
                                next_question_number=question_number + 1)
 
-    if question_number < len(questions):
-        question_data = questions[question_number]
+    if question_number < len(session['questions']):
+        question_data = session['questions'][question_number]
         return render_template('question.html',
+                               score=session['score'],
                                question_number=question_number,
-                               question_text=question_data['question_text'],
+                               question_text=question_data['question'],
                                options=question_data['options'])
 
     # If there are no more questions, redirect to the result page
